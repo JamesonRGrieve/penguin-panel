e5a24e5772131cf9ff6393f86d110d81cacc4625
Boy132 <Boy132@users.noreply.github.com>
2026-08-24 23:56:44 +0200

    Ask for `APP_URL` in setup command to fix css issues (#2535)

--- app/Console/Commands/Environment/AppSettingsCommand.php
+++ app/Console/Commands/Environment/AppSettingsCommand.php
@@ -2,31 +2,86 @@
 
 namespace App\Console\Commands\Environment;
 
+use App\Traits\EnvironmentWriterTrait;
+use Exception;
 use Illuminate\Console\Command;
 
 class AppSettingsCommand extends Command
 {
+    use EnvironmentWriterTrait;
+
     protected $description = 'Configure basic environment settings for the Panel.';
 
-    protected $signature = 'p:environment:setup';
+    protected $signature = 'p:environment:setup
+                            {--url= : The URL that this Panel is running on.}';
 
-    public function handle(): void
+    public function handle(): int
     {
         $path = base_path('.env');
         if (!file_exists($path)) {
             $this->comment('Copying example .env file');
-            copy($path . '.example', $path);
+
+            if (!copy($path . '.example', $path)) {
+                return 1;
+            }
+        }
+
+        if (!$this->handleAppUrl()) {
+            return 1;
         }
 
         if (!config('app.key')) {
             $this->comment('Generating app key');
-            $this->call('key:generate');
+            $return = $this->call('key:generate');
+            if ($return !== 0) {
+                return $return;
+            }
         }
 
         $this->comment('Creating storage link');
-        $this->call('storage:link');
+        $return = $this->call('storage:link');
+        if ($return !== 0) {
+            return $return;
+        }
 
         $this->comment('Caching components & icons');
-        $this->call('filament:optimize');
+        $return = $this->call('filament:optimize');
+        if ($return !== 0) {
+            return $return;
+        }
+
+        return 0;
+    }
+
+    private function handleAppUrl(): bool
+    {
+        $appUrl = $this->option('url');
+
+        if (blank($appUrl) && $this->input->isInteractive()) {
+            $appUrl = $this->ask('Application URL', config('app.url'));
+        }
+
+        if (blank($appUrl)) {
+            $this->error('Application URL is required.');
+
+            return false;
+        }
+
+        if (!str_starts_with($appUrl, 'http://') && !str_starts_with($appUrl, 'https://')) {
+            $this->error('Application URL need to start with either http:// or https://.');
+
+            return false;
+        }
+
+        try {
+            $this->comment('Writing APP_URL to .env file');
+            $this->writeToEnvironment(['APP_URL' => $appUrl]);
+
+            return true;
+        } catch (Exception $exception) {
+            $this->error('Could not write .env file: ' . $exception->getMessage());
+
+            return false;
+        }
     }
 }
